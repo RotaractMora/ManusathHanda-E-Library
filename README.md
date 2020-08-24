aa2fe5545bf58e67aef9731a79f6d9d8da6ef9f3
Rukshan J. Senanayaka <43839071+RukshanJS@users.noreply.github.com>
2020-08-24 21:30:33 +0530

    scripts

---
+++ README.md
@@ -0,0 +1,3 @@
+# RotaractMora Manusath Handa Website
+Earlier website manusath.netlify.app is depreciated.<br />
+New website is [rotamanusath.herokuapp.com](https://rotamanusath.herokuapp.com)
---
+++ test.js
@@ -0,0 +1,8 @@
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
